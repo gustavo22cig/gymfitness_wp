c191ac1b9d59095a3ee2b5e582f3953f0a46ab6f
gustavo22cig <gustavo22cig@gmail.com>
2023-11-10 17:48:13 -0400

    formato general style - normalize

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -15,6 +15,7 @@ add_action('init', 'gymfitness_manus');
 function gymfitness_scripts_styles(){
 
     //definimos el nombre, ubicacion, dependencias, actualizar la version que se encuentra en la cache
-    wp_enqueue_style('style',get_stylesheet_uri(), array(), '1.0.0');
+    wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
+    wp_enqueue_style('style',get_stylesheet_uri(), array('normalize'), '1.0.0');
 }
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
